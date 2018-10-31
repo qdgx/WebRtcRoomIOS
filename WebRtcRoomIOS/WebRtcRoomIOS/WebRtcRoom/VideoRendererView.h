f26dccc4ab7e1499c9592762b439f1b6e46c5dda
Mr.Wang <wangjiarong@nationsky.com>
2018-10-31 09:12:32 +0800

    添加新的view

---
+++ WebRtcRoomIOS/WebRtcRoomIOS/WebRtcRoom/VideoRendererView.h
@@ -0,0 +1,17 @@
+//
+//  VideoRendererView.h
+//  WebRtcRoomIOS
+//
+//  Created by wjr on 2018/10/31.
+//  Copyright © 2018 wjr. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface VideoRendererView : UIView
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ WebRtcRoomIOS/WebRtcRoomIOS/WebRtcRoom/VideoRendererView.m
@@ -0,0 +1,21 @@
+//
+//  VideoRendererView.m
+//  WebRtcRoomIOS
+//
+//  Created by wjr on 2018/10/31.
+//  Copyright © 2018 wjr. All rights reserved.
+//
+
+#import "VideoRendererView.h"
+
+@implementation VideoRendererView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
